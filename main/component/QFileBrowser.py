e5e66394ee7d710d8f4f1c62b67fa8143a82c511
Timothy Halim <timothy.halim@live.com>
2021-03-12 00:43:18 +0700

    Update FileBrowser

--- main/component/QFileBrowser.py
+++ main/component/QFileBrowser.py
@@ -2,12 +2,16 @@ import sys
 import os
 
 try:
+    from PySide.QtGui  import *
+    from PySide.QtCore import *
+    qttype = "PySide"
+except:
     from PySide2.QtWidgets import *
     from PySide2.QtGui     import *
     from PySide2.QtCore    import *
-except:
-    from PySide.QtGui  import *
-    from PySide.QtCore import *
+    qttype = "PySide2"
+
+print(qttype)
 
 class MyFileSystemModel(QFileSystemModel):
     def __init__(self, h_align = Qt.AlignLeft, v_align = Qt.AlignVCenter, parent = None):
@@ -34,14 +38,28 @@ class MyFileSystemModel(QFileSystemModel):
             return super(MyFileSystemModel, self).data(index, role)
 
 
-class Widget(QWidget):
+class FileBrowser(QWidget):
     def __init__(self, *args, **kwargs):
         QWidget.__init__(self, *args, **kwargs)
+        
+        path = "?"
+
         self.setMinimumWidth(800)
 
         hlay = QHBoxLayout(self)
+
+        # Drive List
         self.driveList = QListView()
         self.driveList.setMaximumWidth(180)
+
+        self.dirModel = QFileSystemModel()
+        self.dirModel.setRootPath(QDir.rootPath())
+        self.dirModel.setFilter(QDir.NoDotAndDotDot | QDir.AllDirs)
+        self.driveList.setModel(self.dirModel)
+        # self.driveList.setRootIndex(self.dirModel.index(path))
+        index = self.dirModel.index("C:/")
+        # self.driveList.selectionModel().setCurrentIndex(0,0)
+        
         hlay.addWidget(self.driveList)
         
         vlay = QVBoxLayout(self)
@@ -60,59 +78,87 @@ class Widget(QWidget):
         vlay.addWidget(self.fileList)
         vlay.addWidget(self.currentFile)
 
-        path = "c"
-
-        self.dirModel = QFileSystemModel()
-        self.dirModel.setRootPath(QDir.rootPath())
-        self.dirModel.setFilter(QDir.NoDotAndDotDot | QDir.AllDirs)
-
         self.fileModel = MyFileSystemModel()
-        self.fileModel.setFilter(QDir.NoDot |  QDir.Files | QDir.AllDirs)
+        self.fileModel.setFilter(QDir.NoDot |  QDir.Files | QDir.AllDirs | QDir.Drives)
         self.fileModel.setNameFilters(["*.ma"])
         self.fileModel.setNameFilterDisables(False)
-
-        self.driveList.setModel(self.dirModel)
         self.fileList.setModel(self.fileModel)
-
-        self.driveList.setRootIndex(self.dirModel.index(path))
         self.fileList.setRootIndex(self.fileModel.index(path))
 
         header = self.fileList.horizontalHeader()
-        # header.setSectionResizeMode(QHeaderView.ResizeToContents)   
-        header.setSectionResizeMode(0, QHeaderView.Stretch)
-        header.setDefaultAlignment(Qt.AlignVCenter)
+        try:
+            header.setSectionResizeMode(0, QHeaderView.Stretch)
+        except:
+            header.setResizeMode(0, QHeaderView.Stretch)
         self.fileList.setColumnWidth(1,80)
         self.fileList.setColumnWidth(2,80)
 
         self.driveList.clicked.connect(self.drive_click)
-        self.fileList.selectionModel().selectionChanged.connect(self.file_click)
+        # self.fileList.selectionModel().selectionChanged.connect(self.change_selection)
+        # self.fileList.clicked.connect(self.change_selection)
         self.fileList.doubleClicked.connect(self.file_expand)
         self.currentPath.textChanged.connect(self.change_path)
+
+        self.fileList.selectionModel().currentChanged.connect(self.selection_changed)
 
     def drive_click(self, index):
         path = self.dirModel.fileInfo(index).absoluteFilePath()
         self.fileList.setRootIndex(self.fileModel.setRootPath(path))
         self.currentPath.setText(path)
 
-    def file_click(self, selection):
-        path = self.fileModel.fileInfo(selection.indexes()[0]).absoluteFilePath()
-        if self.fileModel.fileInfo(selection.indexes()[0]).isFile():
-            self.currentFile.setText(os.path.basename(path))
+    def selection_changed(self, index, *args):
+        print("selected: ", )
+        self._currentPath = os.path.join( self.fileModel.rootPath(), index.data() ).replace("\\","/")
+
+    # def change_selection(self, index):
+    #     path = self.fileModel.fileInfo(index).absoluteFilePath()
+    #     if self.fileModel.fileInfo(index).isFile():
+    #         self.currentFile.setText(os.path.basename(path))
 
     def file_expand(self, index):
+        print("DBLCLICK")
         path = self.fileModel.fileInfo(index).absoluteFilePath()
         if self.fileModel.fileInfo(index).isDir():
             self.currentPath.setText(path)
+
+        print(path)
     
     def change_path(self):
-        path = os.path.normpath(self.currentPath.text()).replace("\\", "/")
-        if os.path.isdir(path):
-            self.fileList.setRootIndex(self.fileModel.setRootPath(path))
-        elif os.path.isfile(path):
-            self.fileList.setRootIndex(self.fileModel.setRootPath(os.path.dirname(path)))
-            self.currentFile.setText(os.path.basename(path))
-            self.currentPath.setText(os.path.dirname(path))
+        if self.currentPath.text():
+            path = os.path.normpath(self.currentPath.text()).replace("\\", "/")
+            if os.path.isdir(path):
+                self.fileList.setRootIndex(self.fileModel.setRootPath(path))
+            elif os.path.isfile(path):
+                self.fileList.setRootIndex(self.fileModel.setRootPath(os.path.dirname(path)))
+                index = self.fileModel.index(path)
+                print(index)
+                self.fileList.selectionModel().setCurrentIndex(index, 0)
+                self.currentFile.setText(os.path.basename(path))
+                self.currentPath.setText(os.path.dirname(path))
+        else:
+            self.fileList.setRootIndex(self.fileModel.setRootPath("."))
+            self.currentFile.setText("")
+            self.currentPath.setText("")
 
+    def keyPressEvent(self, event):
+        #super(FileBrowser).keyPressEvent(event)
+        print(event.key())
+        if event.key() == Qt.Key_Enter: 
+            print("Enter")
+        elif event.key() == Qt.Key_Return:
+            print("Return")
+            if os.path.isdir(self._currentPath):
+                self.currentPath.setText(self._currentPath)
+                self.currentFile.setText("")
+            elif os.path.isfile(self._currentPath):
+                self.currentPath.setText(os.path.dirname(self._currentPath))
+                self.currentFile.setText(os.path.basename(self._currentPath))
 
-w = Widget()
+        # (
+        #     Qt.Key_Tab,
+        #     Qt.Key_Return,
+        #     Qt.Key_Enter,
+        # ):
+
+w = FileBrowser()
 w.show()
